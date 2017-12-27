e27e30312d7690ebe786429caff1bbf7e05e4aa1
WJA <wja_1993@163.com>
2017-12-27 18:24:23 +0800

    ensemble

--- ensemble.py
+++ ensemble.py
@@ -9,6 +9,8 @@ from sklearn.metrics import mean_squared_error, make_scorer
 from xgboost import XGBRegressor
 
 from sklearn.model_selection import train_test_split
+import os
+import config
 
 
 def mean_squared_error_(ground_truth, predictions):
@@ -113,11 +115,11 @@ class Ensemble(object):
         #     'subsample': [0.72, 0.75, 0.78]
         # }
         param_grid = {
-            'n_estimators': [30, 50, 100, 200],
+            'n_estimators': [52],
             'learning_rate': [0.1],
-            'subsample': [0.6, 0.7, 0.8]
-            'colsample_bytree' : [0.6, 0.8] 
-            'max_depth' = [2,3 ]
+            'subsample': [0.8],
+            'colsample_bytree' : [0.7],
+            'max_depth' : [2]
         }
         grid = GridSearchCV(estimator=self.stacker, param_grid=param_grid, n_jobs=1, cv=5, verbose=20, scoring=MSE)
         grid.fit(S_train, y)
@@ -138,6 +140,27 @@ class Ensemble(object):
         except:
             pass
 
+         #手动调参
+        # from imp import reload 
+        # while True:
+        #     param_grid = config.ensemble_config
+        #     model = GridSearchCV(estimator = self.stacker, param_grid = param_grid, n_jobs = 1, cv=5, verbose=20, scoring = MSE)
+        #     model.fit(S_train, y)
+        
+        #     # print('--- Grid Search Completed: %s minutes ---' % round(((time.time() - start_time) / 60), 2))
+        #     print('Param grid:')
+        #     print(param_grid)
+        #     print('Best Params:')
+        #     print(model.best_params_)
+        #     print('Best CV Score:')
+        #     print(-model.best_score_)
+            
+        #     str = input("Modify the parameter: ")
+        #     if str ==  's':
+        #         break
+        #     else:
+        #         reload(config)
+
         print('--- Stacker Trained: %s minutes ---' % round(((time.time() - start_time) / 60), 2))
 
         y_pred = grid.predict(S_test)[:]
@@ -152,7 +175,7 @@ def main(data_file ='feature_selected_3000.xlsx'):
     test_data = pd.read_excel(data_file, sheet_name = 1)
     test_index = test_data.index
     X_test = test_data.values[:,:]
-    answer_file = "answer_" + data_file.split('.')[0] + ".csv"
+    answer_file = "ensemble_answer_" + os.path.basename(data_file).split('.')[0] + ".csv"
 
     x_train, x_test, y_train, y_test = train_test_split(X_train, Y_train, random_state = 1024, test_size=0.1)
 
@@ -161,10 +184,10 @@ def main(data_file ='feature_selected_3000.xlsx'):
 
     base_models = [
         RandomForestRegressor(random_state = 0, 
-                            n_estimators=500, 
+                            n_estimators=151, 
                             min_samples_split = 2,
                             min_samples_leaf = 2,
-                            max_features = 0.8,
+                            max_features = 0.79,
                             max_depth = 8
         ),
         # ExtraTreesRegressor(
@@ -173,16 +196,16 @@ def main(data_file ='feature_selected_3000.xlsx'):
         # ),
         GradientBoostingRegressor(learning_rate = 0.1, 
                                 random_state = 0, 
-                                n_estimators=500, 
+                                n_estimators=28, 
                                 min_samples_split = 2,
-                                min_samples_leaf = 6,
-                                max_features = 0.8,
+                                min_samples_leaf = 5,
+                                max_features = 0.81,
                                 subsample = 0.8,
                                 max_depth = 6
         ),
         XGBRegressor(
             random_state = 0,
-            n_estimators=47, 
+            n_estimators=49, 
             max_depth=3,
             learning_rate=0.1, 
             subsample=0.78, 
@@ -190,16 +213,17 @@ def main(data_file ='feature_selected_3000.xlsx'):
         )
     ]
     ensemble = Ensemble(
-        n_folds=5,
-        stacker=XGBRegressor(learning_rate = 0.1)
-        base_models=base_models
+        n_folds = 5,
+        stacker = XGBRegressor(learning_rate = 0.1),
+        base_models = base_models
     )
 
     y_pred = ensemble.fit_predict(X=X_train, y=Y_train, T=X_test)
 
-    # pd.DataFrame({'id': test_index, 'relevance': y_pred}).to_csv('answer_ensemble.csv', index=False, header = False)
+    # pd.DataFrame({'id': test_index, 'y': y_pred}).to_csv(os.path.join(config.base_path, "answer", answer_file, index=False, header = False))
 
     print('--- Submission Generated: %s minutes ---' % round(((time.time() - start_time) / 60), 2))
 
 if __name__ == '__main__':
-    main()
+    data_file = "E:\\天池比赛\\IM-contest\\data\\feature_selected_A_3000.xlsx"
+    main(data_file)
